6f1674afde9b3f57e222522a6da9901369f85409
webbE <anton.wahlberg@edelan.se>
2017-02-15 23:52:12 +0100

    Created the ChooseNextWaypoint as a C++ class that inherits from BTTaskNode. Changed the Behavior Tree so it uses the C++ class instead of the blueprint.

--- Source/TestingGround/TestingGround.Build.cs
+++ Source/TestingGround/TestingGround.Build.cs
@@ -6,6 +6,6 @@ public class TestingGround : ModuleRules
 {
 	public TestingGround(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "GameplayTasks" });
 	}
 }
